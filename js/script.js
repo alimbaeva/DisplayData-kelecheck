1c6eeb3e9e06b96f9fde32cc0df27a9c3a9b5744
Asel Alimbaeva <asel_19.86@mail.ru>
2021-11-07 15:22:48 +0300

    change code in the file js

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ const products = [
   },
   {
     id: 3,
-    title: "Hello",
+    title: "Helllo",
     category: "laptops",
     price: 60000,
     img: "https://www.kivano.kg/images/product/68352/full/1622355077_67139000.png",
@@ -31,15 +31,48 @@ const products = [
     img: "https://www.kivano.kg/images/product/92929/full/1621780991_50190300.jpg",
     desc: "разрешение: 4K UHD (3840x2160), HDR диагональ экрана: 65 частота обновления экрана: 100 Гц формат HDR: Dolby Vision, HDR 10 Pro мощность звука: 40 Вт (2х10 + 2х10 Вт) платформа Smart TV: webOS",
   },
+  {
+    id: 5,
+    title: "Телевизор LG LG OLED65CXRLA",
+    category: "tv",
+    price: 569980,
+    img: "https://www.kivano.kg/images/product/92929/full/1621780991_50190300.jpg",
+    desc: "разрешение: 4K UHD (3840x2160), HDR диагональ экрана: 65 частота обновления экрана: 100 Гц формат HDR: Dolby Vision, HDR 10 Pro мощность звука: 40 Вт (2х10 + 2х10 Вт) платформа Smart TV: webOS",
+  },
+  {
+    id: 6,
+    title: "Xiaomi Mi 1000 Lite",
+    category: "phones",
+    price: 90700,
+    img: "https://www.kivano.kg/images/product/92416/full/1635945551_75038600.jpg",
+    desc: "экран: 6.55 (2400x1080) 90 Гц, оперативная память: 8 ГБ, память: 128 ГБ, слот для карты памяти, 3 камеры: 64 МП, 8 МП, 5 МП, аккумулятор: 4250 мА·ч, процессор: Qualcomm Snapdragon 780G, SIM-карты: 2 (nano SIM), операционная система: Android 11, беспроводные интерфейсы: NFC, Wi-Fi, Bluetooth 5.1, интернет: 5G, 4G LTE, вес: 159 г",
+  }
 ];
-
 
 
 window.addEventListener("load", function () {
   const productsInner = document.querySelector(".products__inner");
+  const productsBtn = document.querySelector(".products__btn");
+
+
+  var numBtn = [];
+  function funNumBtn() {
+    products.map(function (item) {
+      if (numBtn.indexOf(item.category) === -1) {
+        numBtn.push(item.category)
+        const button = this.document.createElement('button');
+        button.classList = 'products__filters-btn';
+        button.type = "button";
+        button.dataset.catigory = `${item.category}`;
+        button.textContent = `${item.category}`;
+        productsBtn.append(button);
+      }
+    })
+  }
+
+  funNumBtn();
   const filterBtn = document.querySelectorAll(".products__filters-btn");
 
-  displayProductsItems(products);
 
   filterBtn.forEach(function (btn) {
     btn.addEventListener('click', function (e) {
@@ -60,30 +93,87 @@ window.addEventListener("load", function () {
   });
 
 
+
   function displayProductsItems(products) {
     let displayProducts = products.map(function (item) {
       return `<article key="${item.id}" class="products__item">
-        <img
-          class="products__item-photo"
-          src="${item.img}"
-          alt=""
-        />
-        <div class="products__item-info">
-          <header>
-            <h3>${item.title}</h3>
-            <span class="products__item-price">${item.price}сом</span>
-          </header>
-          <p class="products__item-text">
-            ${item.desc}
-          </p>
-        </div>
-      </article>`;
-    });
-
+            <img
+              class="products__item-photo"
+              src="${item.img}"
+              alt=""
+            />
+            <div class="products__item-info">
+              <header>
+                <h3>${item.title}</h3>
+                <span class="products__item-price">${item.price}сом</span>
+              </header>
+              <p class="products__item-text">
+                ${item.desc}
+              </p>
+            </div>
+          </article>`;
+    })
     displayProducts = displayProducts.join("");
     productsInner.innerHTML = displayProducts;
   }
+
+  console.log(filterBtn);
+  displayProductsItems(products);
+
 });
+
+
+
+
+// window.addEventListener("load", function () {
+//   const productsInner = document.querySelector(".products__inner");
+//   const filterBtn = document.querySelectorAll(".products__filters-btn");
+
+//   displayProductsItems(products);
+
+//   filterBtn.forEach(function (btn) {
+//     btn.addEventListener('click', function (e) {
+//       const categoryBtn = e.currentTarget.dataset.catigory;
+
+//       const productCategory = products.filter(function (item) {
+//         if (item.category === categoryBtn) {
+//           return item;
+//         }
+//       });
+
+//       if (categoryBtn === 'all') {
+//         displayProductsItems(products);
+//       } else {
+//         displayProductsItems(productCategory)
+//       }
+//     });
+//   });
+
+
+//   function displayProductsItems(products) {
+//     let displayProducts = products.map(function (item) {
+//       return `<article key="${item.id}" class="products__item">
+//         <img
+//           class="products__item-photo"
+//           src="${item.img}"
+//           alt=""
+//         />
+//         <div class="products__item-info">
+//           <header>
+//             <h3>${item.title}</h3>
+//             <span class="products__item-price">${item.price}сом</span>
+//           </header>
+//           <p class="products__item-text">
+//             ${item.desc}
+//           </p>
+//         </div>
+//       </article>`;
+//     });
+
+//     displayProducts = displayProducts.join("");
+//     productsInner.innerHTML = displayProducts;
+//   }
+// });
 
 
 // // -------------------------------------------------
